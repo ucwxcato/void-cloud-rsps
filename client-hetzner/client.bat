61c7095774dc5828258848b606876992662f05fd
alexfemcat <heittomies@gmail.com>
2026-09-23 15:51:52 +0300

    Fix Hetzner client server arguments

--- client-hetzner/client.bat
+++ client-hetzner/client.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d "%~dp0"
-java -Dvoid.server=2.28.141.196 -Dsun.java2d.uiScale=1.0 -Dsun.java2d.dpiaware=false -jar void-client-1.2.0.jar
+java -Dsun.java2d.uiScale=1.0 -Dsun.java2d.dpiaware=false -jar void-client-1.2.0.jar -ip 2.28.141.196 -p 43594
 pause
